e3722b39ea9e01f508245f37f5a028d6c30280cd
Jesus <jhuanach@everis.com>
2019-07-28 18:31:34 -0500

    Remove packages

--- domain/src/main/java/com/example/domain/bean/ConfigurationEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.bean
-
-import com.example.themovieapp.data.entity.ImageEntity
-
-data class Configuration(
-    val imageEntity: ImageEntity?,
-    val changeKeys: List<String>?
-)
--- domain/src/main/java/com/example/domain/repository/ConfigurationRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.domain.repository
-
-class ConfigurationRepository {
-    fun fetchConfiguration():
-}
